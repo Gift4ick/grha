57625d4a4f02e440a7a2ae37dcd6d4a3540ad319
Gift4ick <gifthub.2018@gmail.com>
2018-06-06 10:46:55 +0300

    test

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+    {
+    public float speed = 0.3f;
+    public float distance = 8;
+	void Start ()
+    {
+        Debug.Log(speed); //
+        //transform.Translate(1, 0, 0); //сторона отправки объекта
+	}
+	
+	void Update ()
+    {
+        if (transform.position.x >= distance || transform.position.x <= -distance)
+
+        {
+            speed = -speed;
+        }
+     
+        
+        
+        transform.Translate(speed, 0, 0);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10;
+    public float rotspeed = 100;
+    public float jumpSpeed = 100;
+    // 
+    void Start()
+    {
+
+    }
+
+    // 
+    void Update()
+    {
+        float movX = Input.GetAxis("Horizontal") * speed * Time.deltaTime; // управление игрой по горизонтали Horizontal
+        float movZ = Input.GetAxis("Vertical") * speed * Time.deltaTime; // управление игрой вертикально Vertical
+        float rotY = Input.GetAxis("Horizontal") * rotspeed * Time.deltaTime; // управоение игроком по диагонали
+        float movY = 0;
+        if (Input.GetButtonDown("Jump"))
+        {
+            movY = jumpSpeed * Time.deltaTime;
+        }
+        transform.Translate(movX, movY, movZ); // премещаем объект 
+        transform.Rotate(0, rotY, 0); // поворачиваем объект 
+
+
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy") // проверяем объект столкновения на 
+         {
+            Debug.Log("Ты умер");
+            GetComponent<Renderer>().material.color = Color.blue; // получаем компомент у материала 
+            Time.timeScale = 0; // останавливаем время в игре 
+        }
+    }
+}
+
